5227d2384730a3c6b38e776fe47780f760c47065
李靈 <lin_li@cmoney.com.tw>
2023-11-14 17:22:03 +0800

    feat: 增加字串資源 - 增加返回的字串

--- app/src/main/java/com/example/lin_li_tranimg/presentation/compose/CommonTopAppBar.kt
+++ app/src/main/java/com/example/lin_li_tranimg/presentation/compose/CommonTopAppBar.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.lin_li_tranimg.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,7 +21,10 @@ fun CommonTopAppBar(
         title = { Text(title) },
         navigationIcon = {
             IconButton(onClick = onNavigationClick) {
-                Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                Icon(
+                    Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(id = R.string.back)
+                )
             }
         }
     )
