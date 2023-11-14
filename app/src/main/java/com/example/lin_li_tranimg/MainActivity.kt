8c44b1f8221a454d64119dd562981b5e11b907cd
李靈 <lin_li@cmoney.com.tw>
2023-11-14 17:46:37 +0800

    feat: 刪除無用的類與變數 - 刪除LoginViewModelFactory - 刪除MainActivity的viewModel變數

--- app/src/main/java/com/example/lin_li_tranimg/MainActivity.kt
+++ app/src/main/java/com/example/lin_li_tranimg/MainActivity.kt
@@ -7,15 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.lin_li_tranimg.presentation.viewmodel.LoginViewModel
 import com.example.lin_li_tranimg.ui.theme.Lin_li_tranimgTheme
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MainActivity : ComponentActivity() {
-    // Lazy inject LoginViewModel
-    private val loginViewModel: LoginViewModel by viewModel()
-
     companion object {
         const val TAG = "LinLi"
     }
